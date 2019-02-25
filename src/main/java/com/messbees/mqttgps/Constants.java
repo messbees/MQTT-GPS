f98346eb9ec9573ff1b694bc6216a1ac57935e85
Anton Betsun <messbees@gmail.com>
2019-02-26 02:44:06 +0300

    Refactor.

--- src/main/java/com/messbees/mqttgps/Constants.java
+++ src/main/java/com/messbees/mqttgps/Constants.java
@@ -1,9 +1,9 @@
 package com.messbees.mqttgps;
 
 public class Constants {
-    public static final String BROKER_URL = "";
 
-    public static final String TOPIC = "";
+    public static final String BROKER_URL = "tcp://db3gw.keenetic.pro:8883";
 
+    public static final String TOPIC = "/Register/geo";
 
 }
