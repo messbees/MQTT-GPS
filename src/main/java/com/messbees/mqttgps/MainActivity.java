b4a383fc1be619bbc9541c94aa992ce4c007d282
Anton Betsun <messbees@gmail.com>
2019-02-25 23:48:20 +0300

    Add onclick listener.

--- src/main/java/com/messbees/mqttgps/MainActivity.java
+++ src/main/java/com/messbees/mqttgps/MainActivity.java
@@ -2,12 +2,28 @@ package com.messbees.mqttgps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gps_button:
+
+                break;
+            case R.id.post_button:
+
+                break;
+        }
+    }
+
 }
